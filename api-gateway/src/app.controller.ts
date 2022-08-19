e412c9d2412c0edec914fac6880fafc0e9f6530d
Lucas Mazaron <lucas.mazaron@outlook.com>
2022-08-19 09:44:06 -0300

    first commit

---
+++ api-gateway/src/app.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Logger, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+import {
+  ClientProxy,
+  ClientProxyFactory,
+  Transport,
+} from '@nestjs/microservices';
+import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
+
+@Controller('api/v1')
+export class AppController {
+  private readonly logger = new Logger(AppController.name);
+  private readonly clientAdminBackend: ClientProxy = ClientProxyFactory.create({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://guest:guest@localhost:5672/smartranking'],
+      queue: 'micro-admin-backend',
+    },
+  });
+
+  @Post('categorias')
+  @UsePipes(ValidationPipe)
+  async criarCategoria(@Body() criarCategoriaDto: CriarCategoriaDto) {
+    return await this.clientAdminBackend.emit(
+      'criar-categoria',
+      criarCategoriaDto,
+    );
+  }
+}
